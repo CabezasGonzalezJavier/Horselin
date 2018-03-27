bc238059022c4f468ebff0544f280b8c3e4d9397
Cabezas <javiergonzalezcabezas@gmail.com>
2018-03-27 23:00:53 +0100

    added TaskPresenter

--- app/src/main/java/com/thedeveloperworldisyours/horselin/task/TasksPresenter.kt
+++ app/src/main/java/com/thedeveloperworldisyours/horselin/task/TasksPresenter.kt
@@ -1,6 +1,68 @@
 package com.thedeveloperworldisyours.horselin.task
 
+import com.thedeveloperworldisyours.horselin.BasePresenter
+import com.thedeveloperworldisyours.horselin.BaseView
+import com.thedeveloperworldisyours.horselin.Task
+
 /**
  * Created by javiergonzalezcabezas on 26/3/18.
  */
-class TasksPresenter{}
+interface TasksPresenter {
+
+    interface View : BaseView<Presenter> {
+
+        var isActive: Boolean
+
+        fun setLoadingIndicator(active: Boolean)
+
+        fun showTasks(tasks: List<Task>)
+
+        fun showAddTask()
+
+        fun showTaskDetailsUi(taskId: String)
+
+        fun showTaskMarkedComplete()
+
+        fun showTaskMarkedActive()
+
+        fun showCompletedTasksCleared()
+
+        fun showLoadingTasksError()
+
+        fun showNoTasks()
+
+        fun showActiveFilterLabel()
+
+        fun showCompletedFilterLabel()
+
+        fun showAllFilterLabel()
+
+        fun showNoActiveTasks()
+
+        fun showNoCompletedTasks()
+
+        fun showSuccessfullySavedMessage()
+
+        fun showFilteringPopUpMenu()
+    }
+
+    interface Presenter : BasePresenter {
+
+        var currentFiltering: TasksFilterType
+
+        fun result(requestCode: Int, resultCode: Int)
+
+        fun loadTasks(forceUpdate: Boolean)
+
+        fun addNewTask()
+
+        fun openTaskDetails(requestedTask: Task)
+
+        fun completeTask(completedTask: Task)
+
+        fun activateTask(activeTask: Task)
+
+        fun clearCompletedTasks()
+    }
+
+}
